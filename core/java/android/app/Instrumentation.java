7b2bcaf7fb63e49f5f79e3d1ec24d40d141170bb
huyang <1184394624@qq.com>
2026-09-15 09:21:42 +0800

    Parallel World: 1st version
    1.Use SystemTaskFragmentOrganizer to manager TaskFragments;
    2.Use magic window config file;
    3.Some hardcode for com.tencent.mm;

--- core/java/android/app/Instrumentation.java
+++ core/java/android/app/Instrumentation.java
@@ -2050,6 +2050,21 @@ public class Instrumentation {
             Log.d(TAG, "parallel world: put extra config for " + packageName + ": " + result);
             intent.setExtraFDE(result);
             return true;
+        } else if( TextUtils.contains("com.jingdong.app.mall")
+                || TextUtils.contains("com.ss.android.article.news")
+                || TextUtils.contains("com.zhihu.android")
+                || TextUtils.contains("com.xingin.xhs")
+                || TextUtils.contains("com.sina.weibo")
+                || TextUtils.contains("com.moji.mjweather")
+                || TextUtils.contains("com.kuaishou.nebula")
+                || TextUtils.contains("ctrip.android.view")
+                || TextUtils.contains("com.Qunar")
+                || TextUtils.contains("com.tencent.mm")
+                || TextUtils.contains("com.lite.ceclanxin")
+            ){
+            Log.d(TAG, "parallel world: put extra config for " + packageName + ": " + "{\"ratio\":\"4:5\"}");
+            intent.setExtraFDE("{\"ratio\":\"4:5\"}");
+            return true;
         }
         return false;
     }
